da84e148fc36be1af023ffc3ddc9fe19701b04bf
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-17 00:26:52 +0600

    refactor: code optimaize and clean codebase

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -2,10 +2,10 @@ import Navbar from '../ui/Navbar';
 
 export default function Header() {
     return (
-        <header className='w-full py-5 px-5 md:px-10 sticky top-0 left-0 bg-white z-30'>
+        <header className='w-full bg-gray-950 py-4 px-5 md:px-10 sticky top-0 left-0 z-30'>
             <div className='w-full max-w-[1300px] mx-auto flex items-center justify-between'>
                 <div className='w-fit'>
-                    <h1 className='text-2xl text-gray-950 font-semibold'>
+                    <h1 className='text-2xl text-white font-semibold'>
                         Sport<span className='text-green-500'>Nest</span>
                     </h1>
                 </div>
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -3,11 +3,8 @@
 import Link from 'next/link';
 
 import { IoMenu, IoClose } from 'react-icons/io5';
-import { motion, AnimatePresence } from 'motion/react';
-import { ClickAwayListener } from '@mui/material';
-
-import { useState } from 'react';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Navbar() {
     const pathname = usePathname();
@@ -19,7 +16,7 @@ export default function Navbar() {
     };
 
     return (
-        <nav>
+        <nav className='w-fit'>
             <button
                 type='button'
                 role='button'
@@ -27,89 +24,138 @@ export default function Navbar() {
                 onClick={handleToggleMenu}
                 className='flex items-center justify-center cursor-pointer'
             >
-                <IoMenu size={28} className='text-gray-950' />
+                <IoMenu
+                    size={28}
+                    className='text-white transition-colors duration-200 ease-linear hover:text-green-500'
+                />
             </button>
-            <AnimatePresence>
-                {toggleMenu && (
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        className='fixed inset-0 bg-gray-950/20 z-40'
-                    >
-                        <ClickAwayListener onClickAway={handleToggleMenu}>
-                            <motion.div
-                                initial={{ x: '100%' }}
-                                animate={{ x: 0 }}
-                                exit={{ x: '100%' }}
-                                transition={{ duration: 0.3 }}
-                                className='absolute w-full max-w-[400px] h-screen bg-white right-0 z-50'
+            <div
+                className={`fixed inset-0 bg-gray-950/20 transition-all duration-200 ease-linear ${toggleMenu ? 'visible opacity-100' : 'invisible opacity-0 delay-500'} z-40`}
+            >
+                <div
+                    className={`fixed top-0 right-0 w-full max-w-[350px] h-screen px-5 bg-gray-950 transition-all duration-200 ease-linear delay-300 ${toggleMenu ? 'translate-x-0' : 'translate-x-full'} z-50`}
+                >
+                    <div className='w-full h-[64px] flex items-center'>
+                        <button
+                            type='button'
+                            role='button'
+                            aria-label='button'
+                            onClick={handleToggleMenu}
+                            className='flex items-center justify-center cursor-pointer'
+                        >
+                            <IoClose
+                                size={28}
+                                className='text-white transition-colors duration-200 ease-linear hover:text-green-500'
+                            />
+                        </button>
+                    </div>
+                    <div className='w-full mt-8 flex flex-col gap-5'>
+                        <button
+                            type='button'
+                            role='button'
+                            aria-label='button'
+                            onClick={handleToggleMenu}
+                            className='w-fit'
+                        >
+                            <Link
+                                href={'/'}
+                                className={`text-sm text-gray-500 transition-colors duration-200 ease-linear hover:text-green-500 ${pathname === '/' ? 'text-green-500' : 'text-gray-500 hover:text-green-500'}`}
                             >
-                                <div className='w-full p-5'>
-                                    <button
-                                        type='button'
-                                        role='button'
-                                        aria-label='button'
-                                        onClick={handleToggleMenu}
-                                        className='flex items-center justify-center cursor-pointer'
-                                    >
-                                        <IoClose
-                                            size={28}
-                                            className='text-gray-950'
-                                        ></IoClose>
-                                    </button>
-                                    <nav className='w-full mt-10 flex flex-col gap-5'>
-                                        <Link
-                                            href={'/'}
-                                            className={`text-base transition-colors duration-200 ease-linear ${pathname === '/' ? 'text-green-500' : 'text-gray-500 hover:text-gray-950'}`}
-                                        >
-                                            Home
-                                        </Link>
-                                        <div className='w-full h-px bg-gray-200'></div>
-                                        <Link
-                                            href={'/all-facilities'}
-                                            className={`text-base transition-colors duration-200 ease-linear ${pathname === '/all-facilities' ? 'text-green-500' : 'text-gray-500 hover:text-gray-950'}`}
-                                        >
-                                            All facilities
-                                        </Link>
-                                    </nav>
-                                    <div className='w-full flex items-center justify-center gap-5 mt-10'>
-                                        <button
-                                            type='button'
-                                            role='button'
-                                            aria-label='button'
-                                            onClick={handleToggleMenu}
-                                            className='w-full'
-                                        >
-                                            <Link
-                                                href={'/auth/register'}
-                                                className='flex items-center justify-center w-full bg-green-500 cursor-pointer py-[15px] rounded-xl text-base text-white transition-colors duration-200 ease-linear hover:bg-green-400'
-                                            >
-                                                Register
-                                            </Link>
-                                        </button>
-
-                                        <button
-                                            type='button'
-                                            role='button'
-                                            aria-label='button'
-                                            onClick={handleToggleMenu}
-                                            className='w-full'
-                                        >
-                                            <Link
-                                                href={'/auth/login'}
-                                                className='flex items-center justify-center w-full border border-gray-950 cursor-pointer py-[15px] rounded-xl text-base text-gray-950'
-                                            >
-                                                Login
-                                            </Link>
-                                        </button>
-                                    </div>
-                                </div>
-                            </motion.div>
-                        </ClickAwayListener>
-                    </motion.div>
-                )}
-            </AnimatePresence>
+                                Home
+                            </Link>
+                        </button>
+                        <div className='w-full h-px bg-gray-900'></div>
+                        <button
+                            type='button'
+                            role='button'
+                            aria-label='button'
+                            onClick={handleToggleMenu}
+                            className='w-fit'
+                        >
+                            <Link
+                                href={'/facilities'}
+                                className={`text-sm text-gray-500 transition-colors duration-200 ease-linear hover:text-green-500 ${pathname === '/facilities' ? 'text-green-500' : 'text-gray-500 hover:text-green-500'}`}
+                            >
+                                Facilities
+                            </Link>
+                        </button>
+                        <div className='w-full h-px bg-gray-900'></div>
+                        <button
+                            type='button'
+                            role='button'
+                            aria-label='button'
+                            onClick={handleToggleMenu}
+                            className='w-fit'
+                        >
+                            <Link
+                                href={'/bookings'}
+                                className={`text-sm text-gray-500 transition-colors duration-200 ease-linear hover:text-green-500 ${pathname === '/bookings' ? 'text-green-500' : 'text-gray-500 hover:text-green-500'}`}
+                            >
+                                Bookings
+                            </Link>
+                        </button>
+                        <div className='w-full h-px bg-gray-900'></div>
+                        <button
+                            type='button'
+                            role='button'
+                            aria-label='button'
+                            onClick={handleToggleMenu}
+                            className='w-fit'
+                        >
+                            <Link
+                                href={'/create-facility'}
+                                className={`text-sm text-gray-500 transition-colors duration-200 ease-linear hover:text-green-500 ${pathname === '/create-facility' ? 'text-green-500' : 'text-gray-500 hover:text-green-500'}`}
+                            >
+                                Create Facility
+                            </Link>
+                        </button>
+                        <div className='w-full h-px bg-gray-900'></div>
+                        <button
+                            type='button'
+                            role='button'
+                            aria-label='button'
+                            onClick={handleToggleMenu}
+                            className='w-fit'
+                        >
+                            <Link
+                                href={'/manage-facilities'}
+                                className={`text-sm text-gray-500 transition-colors duration-200 ease-linear hover:text-green-500 ${pathname === '/manage-facilities' ? 'text-green-500' : 'text-gray-500 hover:text-green-500'}`}
+                            >
+                                Manage Facilities
+                            </Link>
+                        </button>
+                    </div>
+                    <div className='w-full mt-15 flex items-center justify-center gap-5'>
+                        <button
+                            type='button'
+                            role='button'
+                            aria-label='button'
+                            onClick={handleToggleMenu}
+                        >
+                            <Link
+                                href={'/auth/register'}
+                                className={`text-sm text-gray-500 transition-colors duration-200 ease-linear hover:text-green-500 ${pathname === '/manage-facilities' ? 'text-green-500' : 'text-gray-500 hover:text-green-500'}`}
+                            >
+                                Sign up
+                            </Link>
+                        </button>
+                        <div className='w-px h-5 bg-gray-900'></div>
+                        <button
+                            type='button'
+                            role='button'
+                            aria-label='button'
+                            onClick={handleToggleMenu}
+                        >
+                            <Link
+                                href={'/auth/login'}
+                                className={`text-sm text-gray-500 transition-colors duration-200 ease-linear hover:text-green-500 ${pathname === '/manage-facilities' ? 'text-green-500' : 'text-gray-500 hover:text-green-500'}`}
+                            >
+                                Login
+                            </Link>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 }
