ea7782dc70935b65ecfef1ca7313a47bbd89001e
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-17 13:02:33 +0600

    refactor: code optimaize and clean codebase

--- src/app/manage-facilities/page.jsx
+++ src/app/manage-facilities/page.jsx
@@ -1,26 +1,11 @@
 import ManageSection from '@/components/sections/ManageSection';
-
-export const metadata = {
-    title: 'Manage Facilities - SportNest facilities',
-    description: '',
-};
+import PageHeading from '@/components/ui/PageHeading';
 
 export default function ManageFacilities() {
     return (
-        <section
-            className={`w-full min-h-[calc(100vh-333px)] md:min-h-[calc(100vh-289px)] lg:min-h-[calc(100vh-203px)] px-5 lg:px-10`}
-        >
-            <div
-                className={`w-full min-h-[200px] border-b border-gray-200 flex items-center justify-center lg:min-h-[300px]`}
-            >
-                <div className='w-full max-w-7xl mx-auto text-center'>
-                    <h1 className='text-2xl text-gray-950 lg:text-3xl'>
-                        Manage Facilities
-                    </h1>
-                    <div className='w-[70px] mx-auto h-0.5 bg-green-500 mt-[30px]'></div>
-                </div>
-            </div>
-            <div className='w-full py-20'>
+        <section className='w-full'>
+            <PageHeading title={'Manage Facilities'} />
+            <div className='w-full px-5 py-10 md:px-10 md:py-20'>
                 <div className='w-full max-w-5xl mx-auto'>
                     <ManageSection />
                 </div>
--- src/components/ui/ManageCard.jsx
+++ src/components/ui/ManageCard.jsx
@@ -11,47 +11,44 @@ import {
 
 export default function ManageCard() {
     return (
-        <article className='w-full bg-white border border-gray-300 rounded-xl overflow-hidden transition-transform duration-200 ease-linear flex flex-col md:flex-row hover:-translate-y-1'>
-            <div className='aspect-video md:w-full md:max-w-[250px] bg-gray-100 relative'>
+        <article className='w-full bg-white border border-gray-200 rounded-md overflow-hidden transition-transform duration-200 ease-linear flex flex-col hover:-translate-y-1 md:flex-row'>
+            <div className='aspect-video bg-gray-100 relative md:w-full md:max-w-[250px]'>
                 <div className='w-fit flex items-center justify-center gap-2 absolute top-4 right-4 md:hidden'>
-                    <Link
-                        href={`/facility/update/${'update'}`}
-                        className='text-sm text-green-500 bg-green-100 rounded-full py-1 px-2.5 flex items-center justify-center gap-2 cursor-pointer transition-colors duration-200 ease-linear hover:bg-green-200'
-                    >
+                    <button className='bg-orange-50 text-sm text-orange-500 rounded-full py-1 px-2.5 flex items-center justify-center gap-2 cursor-pointer transition-colors duration-200 ease-linear hover:bg-orange-100'>
                         <FaRegEdit />
                         Edit
-                    </Link>
-                    <button className='text-sm text-red-500 bg-red-100 rounded-full py-1 px-2.5 flex items-center justify-center gap-2 cursor-pointer transition-colors duration-200 ease-linear hover:bg-red-200'>
+                    </button>
+                    <button className='bg-red-50 text-sm text-red-500 rounded-full py-1 px-2.5 flex items-center justify-center gap-2 cursor-pointer transition-colors duration-200 ease-linear hover:bg-red-100'>
                         <FaRegTrashCan />
                         Delete
                     </button>
                 </div>
             </div>
             <div className='w-full p-4'>
-                <div className='w-full flex justify-between gap-4'>
+                <div className='w-full flex justify-between gap-3'>
                     <div className='w-fit'>
-                        <h1 className='text-lg text-gray-950 font-medium'>
+                        <h1 className='text-lg text-gray-950 font-medium md:text-xl'>
                             Green Football Turf Arena
                         </h1>
-                        <div className='flex items-center gap-2'>
+                        <div className='flex items-center gap-2 mt-2'>
                             <FaLocationCrosshairs className='text-sm text-gray-500' />
                             <span className='text-sm text-gray-500'>
                                 Gulshan, Dhaka
                             </span>
                         </div>
                     </div>
-                    <div className='w-fit hidden md:flex items-center gap-2'>
-                        <button className='text-sm text-green-500 bg-green-100 rounded-full py-1 px-2.5 flex items-center justify-center gap-2 cursor-pointer transition-colors duration-200 ease-linear hover:bg-green-200'>
+                    <div className='w-fit hidden gap-2 md:flex lg:items-center'>
+                        <button className='w-fit h-fit bg-orange-50 text-sm text-orange-500 rounded-full py-1 px-2.5 flex items-center justify-center gap-2 cursor-pointer transition-colors duration-200 ease-linear hover:bg-orange-100'>
                             <FaRegEdit />
                             Edit
                         </button>
-                        <button className='text-sm text-red-500 bg-red-100 rounded-full py-1 px-2.5 flex items-center justify-center gap-2 cursor-pointer transition-colors duration-200 ease-linear hover:bg-red-200'>
+                        <button className='w-fit h-fit bg-red-50 text-sm text-red-500 rounded-full py-1 px-2.5 flex items-center justify-center gap-2 cursor-pointer transition-colors duration-200 ease-linear hover:bg-red-100'>
                             <FaRegTrashCan />
                             Delete
                         </button>
                     </div>
                 </div>
-                <div className='w-full mt-4 flex items-center gap-2 flex-wrap'>
+                <div className='w-full mt-4 flex flex-wrap items-center gap-2'>
                     <div className='flex items-center gap-2'>
                         <FaCalendarDays className='text-sm text-green-500 mb-0.5' />
                         <p className='text-sm text-gray-500'>
@@ -67,7 +64,7 @@ export default function ManageCard() {
                         </p>
                     </div>
                 </div>
-                <div className='w-full h-px bg-gray-300 my-4'></div>
+                <div className='w-full h-px bg-gray-200 my-4'></div>
                 <div className='w-full flex items-center justify-between'>
                     <h3 className='text-xl font-semibold text-green-500 flex items-center justify-center'>
                         <FaDollarSign className='mb-0.5' />
@@ -75,7 +72,7 @@ export default function ManageCard() {
                     </h3>
                     <Link
                         href={`/facility/details/${'details'}`}
-                        className='w-fit py-3 px-4 bg-green-500 text-sm text-white rounded-lg transition-colors duration-200 ease-linear hover:bg-green-400'
+                        className='w-fit p-3 bg-green-500 text-sm text-white rounded-md transition-colors duration-200 ease-linear hover:bg-green-400'
                     >
                         See Details
                     </Link>
