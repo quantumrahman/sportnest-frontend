8b749c72c354119744afc386bfdfebb9cc1b013c
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-17 14:28:16 +0600

    refactor: code optimaize and clean codebase

--- src/components/ui/FacilityCard.jsx
+++ src/components/ui/FacilityCard.jsx
@@ -1,41 +1,37 @@
 import Link from 'next/link';
 
-import { FaLocationCrosshairs } from 'react-icons/fa6';
-import { TbCurrencyTaka } from 'react-icons/tb';
+import { FaLocationCrosshairs, FaDollarSign } from 'react-icons/fa6';
 
 export default function FacilityCard() {
     return (
-        <article className='w-full bg-white border border-gray-300 rounded-xl overflow-hidden transition-transform duration-200 ease-linear hover:-translate-y-1'>
-            <div className='aspect-video relative overflow-hidden bg-gray-100'></div>
+        <article className='w-full bg-white border border-gray-200 rounded-md overflow-hidden transition-transform duration-200 ease-linear hover:-translate-y-1'>
+            <div className='aspect-video bg-gray-100 relative overflow-hidden'></div>
             <div className='w-full flex flex-col items-center justify-center p-4'>
                 <div className='w-full'>
                     <span className='text-xs font-medium text-green-500 uppercase'>
                         Football
                     </span>
-                    <h1 className='text-lg font-medium capitalize'>
+                    <h1 className='text-lg font-medium capitalize mt-2 md:text-xl'>
                         Green Football Turf Arena
                     </h1>
                 </div>
-                <div className='w-full flex items-center gap-2 mt-1'>
+                <div className='w-full flex items-center gap-2 mt-1.5'>
                     <FaLocationCrosshairs className='text-sm text-gray-500' />
-                    <span className='block text-sm text-gray-500'>
+                    <span className='text-sm text-gray-500'>
                         Gulshan, Dhaka
                     </span>
                 </div>
-                <div className='w-full h-px bg-gray-300 my-4'></div>
+                <div className='w-full h-px bg-gray-200 my-4'></div>
                 <div className='w-full flex items-center justify-between gap-3'>
                     <div className='w-fit flex justify-center'>
-                        <TbCurrencyTaka className='text-lg text-green-500 mt-1' />
-                        <p className='text-lg font-semibold text-green-500'>
+                        <h3 className='text-xl font-semibold text-green-500 flex items-center justify-center'>
+                            <FaDollarSign className='mb-0.5' />
                             800
-                            <span className='text-[10px] text-gray-500'>
-                                /hr
-                            </span>
-                        </p>
+                        </h3>
                     </div>
                     <Link
-                        href={`/facility/details/${'hhh'}'`}
-                        className='w-fit py-[15px] px-4 bg-green-500 text-sm text-white rounded-lg transition-colors duration-200 ease-linear hover:bg-green-400'
+                        href={`/facility/details/${'details'}`}
+                        className='w-fit p-3 bg-green-500 text-sm text-white rounded-md transition-colors duration-200 ease-linear hover:bg-green-400'
                     >
                         Book Now
                     </Link>
