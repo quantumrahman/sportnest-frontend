c23e39c0d99fa175d4fae3ebd09c44a44970a6d3
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-18 09:32:56 +0600

    refactor: code optimaize and clean codebase

--- src/components/sections/DetailsSection.jsx
+++ src/components/sections/DetailsSection.jsx
@@ -4,22 +4,22 @@ import BookingForm from '../ui/BookingForm';
 export default function DetailsSection() {
     return (
         <section className='w-full'>
-            <div className='aspect-video bg-gray-100 relative overflow-hidden rounded-xl'></div>
+            <div className='aspect-video bg-gray-100 relative overflow-hidden rounded-md'></div>
             <div className='w-full h-px bg-gray-200 my-8'></div>
-            <div className='w-full flex flex-col items-center justify-between gap-4 md:flex-row'>
+            <div className='w-full flex flex-col items-center justify-between gap-10 lg:gap-5 lg:flex-row'>
                 <div className='flex-2'>
                     <div className='w-full flex flex-col'>
                         <span className='text-sm md:text-base text-green-500'>
                             Football
                         </span>
-                        <h1 className='text-xl md:text-2xl lg:text-3xl font-medium mt-2'>
+                        <h1 className='text-xl font-medium mt-2 md:text-2xl lg:text-3xl'>
                             Green Football Turf Arena
                         </h1>
                     </div>
                     <div className='w-full flex items-center flex-wrap gap-3 mt-4'>
                         <div className='flex items-center gap-2'>
-                            <FaLocationDot className='text-sm md:text-base text-green-500 mb-0.5' />
-                            <p className='text-sm md:text-base text-gray-500'>
+                            <FaLocationDot className='text-sm text-green-500 mb-0.5 md:text-base' />
+                            <p className='text-sm text-gray-500 md:text-base'>
                                 Location:{' '}
                                 <span className='text-gray-950'>
                                     Gulshan, Dhaka
@@ -27,8 +27,8 @@ export default function DetailsSection() {
                             </p>
                         </div>
                         <div className='flex items-center gap-2'>
-                            <FaUser className='text-sm md:text-base text-green-500 mb-0.5' />
-                            <p className='text-sm md:text-base text-gray-500'>
+                            <FaUser className='text-sm text-green-500 mb-0.5 md:text-base' />
+                            <p className='text-sm text-gray-500 md:text-base'>
                                 Capacity:{' '}
                                 <span className='text-gray-950'>
                                     22 players
@@ -36,8 +36,8 @@ export default function DetailsSection() {
                             </p>
                         </div>
                         <div className='flex items-center gap-2'>
-                            <FaClock className='text-sm md:text-base text-green-500 mb-0.5' />
-                            <p className='text-sm md:text-base text-gray-500'>
+                            <FaClock className='text-sm text-green-500 mb-0.5 md:text-base' />
+                            <p className='text-sm text-gray-500 md:text-base'>
                                 Time:{' '}
                                 <span className='text-gray-950 uppercase'>
                                     6am-6pm
@@ -46,11 +46,11 @@ export default function DetailsSection() {
                         </div>
                     </div>
                     <div className='w-full mt-8'>
-                        <h4 className='text-base md:text-lg text-gray-950 font-medium'>
+                        <h4 className='text-base text-gray-950 font-medium md:text-lg'>
                             Desrciption
                         </h4>
                         <div className='w-full max-w-[50px] h-0.5 bg-green-500 my-2'></div>
-                        <p className='text-sm md:text-base text-gray-500 mt-5'>
+                        <p className='text-sm text-gray-500 leading-relaxed mt-5 md:text-base'>
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Rerum nobis ab voluptatibus quo labore
                             suscipit autem consequatur, maxime ipsum est aliquid
@@ -61,51 +61,54 @@ export default function DetailsSection() {
                         </p>
                     </div>
                     <div className='w-full mt-8'>
-                        <h4 className='text-base md:text-lg text-gray-950 font-medium'>
+                        <h4 className='text-base text-gray-950 font-medium md:text-lg'>
                             Amenities
                         </h4>
                         <div className='w-full max-w-[50px] h-0.5 bg-green-500 my-2'></div>
                         <div className='w-full flex flex-col gap-1 mt-5'>
                             <div className='w-fit flex items-center gap-2'>
                                 <FaCheck className='text-base text-green-500' />
-                                <span className='text-sm md:text-base text-gray-500'>
+                                <span className='text-sm text-gray-500 md:text-base'>
                                     Parking Area
                                 </span>
                             </div>
                             <div className='w-fit flex items-center gap-2'>
                                 <FaCheck className='text-base text-green-500' />
-                                <span className='text-sm md:text-base text-gray-500'>
+                                <span className='text-sm text-gray-500 md:text-base'>
                                     Changing Room
                                 </span>
                             </div>
                             <div className='w-fit flex items-center gap-2'>
                                 <FaCheck className='text-base text-green-500' />
-                                <span className='text-sm md:text-base text-gray-500'>
+                                <span className='text-sm text-gray-500 md:text-base'>
                                     Field Light
                                 </span>
                             </div>
                             <div className='w-fit flex items-center gap-2'>
                                 <FaCheck className='text-base text-green-500' />
-                                <span className='text-sm md:text-base text-gray-500'>
+                                <span className='text-sm text-gray-500 md:text-base'>
                                     Restaurants
                                 </span>
                             </div>
                             <div className='w-fit flex items-center gap-2'>
                                 <FaCheck className='text-base text-green-500' />
-                                <span className='text-sm md:text-base text-gray-500'>
+                                <span className='text-sm text-gray-500 md:text-base'>
                                     First Aid
                                 </span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className='w-full mt-8 md:mt-0 md:flex-1 bg-white border border-gray-200 rounded-xl p-4'>
+                <div className='w-full mt-8 bg-white border border-gray-200 rounded-md p-4 md:mt-0 md:flex-1'>
                     <div className='w-full mb-4'>
                         <h3 className='text-xl text-gray-950 font-semibold'>
                             Book This Facility
                         </h3>
                     </div>
                     <BookingForm />
+                    <span className='text-sm text-gray-400 block text-center mt-3'>
+                        Free cancellation up to 2 hours before
+                    </span>
                 </div>
             </div>
         </section>
--- src/components/ui/BookingForm.jsx
+++ src/components/ui/BookingForm.jsx
@@ -1,8 +1,12 @@
+'use client';
+
+import { DateField, Label, Description, FieldError } from '@heroui/react';
+
 export default function BookingForm() {
     return (
         <form>
             <label htmlFor='facility-name'>
-                <span className='block text-base text-gray-950'>
+                <span className='block text-sm text-gray-950 md:text-base'>
                     Facility Name
                 </span>
                 <input
@@ -11,58 +15,67 @@ export default function BookingForm() {
                     aria-label='input'
                     name='facility-name'
                     placeholder='e.g. Green turf arena'
-                    defaultValue={'Green Football Turf Arena'}
                     autoComplete='off'
-                    className='w-full outline-none bg-transparent border border-gray-300 rounded-xl py-3 px-3 text-sm text-gray-950 caret-green-500 mt-3 focus:border-green-500 placeholder:text-gray-400'
+                    className='w-full outline-none bg-transparent border border-gray-200 rounded-md py-4 px-5 text-sm text-gray-950 caret-green-500 mt-3 focus:border-green-500 placeholder:text-gray-400'
                 />
-                <div className='w-full h-5'>
+                <div className='w-full h-6'>
                     <span className='text-sm text-red-500'></span>
                 </div>
             </label>
             <label htmlFor='booking-date'>
-                <span className='block text-base text-gray-950'>
+                <span className='block text-sm text-gray-950 mb-3 md:text-base'>
                     Booking Date
                 </span>
-                <input
-                    id='booking-date'
-                    type='text'
-                    aria-label='input'
-                    name='booking-date'
-                    placeholder='e.g. Green turf arena'
-                    autoComplete='off'
-                    className='w-full outline-none bg-transparent border border-gray-300 rounded-xl py-3 px-3 text-sm text-gray-950 caret-green-500 mt-3 focus:border-green-500 placeholder:text-gray-400'
-                />
-                <div className='w-full h-5'>
+                <DateField>
+                    <DateField.Group
+                        id='booking-date'
+                        className='w-full h-[54px] border border-gray-200 rounded-md px-2 flex items-center outline-none ring-0 shadow-none focus:outline-none focus:ring-0 focus-within:border-green-500 focus-within:outline-none focus-within:ring-0 hover:bg-transparent hover:border-green-500'
+                    >
+                        <DateField.Input className='flex w-full items-center gap-1 outline-none text-sm text-gray-950 caret-green-500'>
+                            {(segment) => (
+                                <DateField.Segment
+                                    segment={segment}
+                                    className='rounded-sm outline-none data-[placeholder]:text-gray-400 data-[focused]:bg-green-100 data-[focused]:text-green-700'
+                                />
+                            )}
+                        </DateField.Input>
+                    </DateField.Group>
+                </DateField>
+                <div className='w-full h-6'>
                     <span className='text-sm text-red-500'></span>
                 </div>
             </label>
             <label htmlFor='time-slot'>
-                <span className='block text-base text-gray-950'>Time Slot</span>
+                <span className='block text-sm text-gray-950 md:text-base'>
+                    Time Slot
+                </span>
                 <input
                     id='time-slot'
                     type='text'
                     aria-label='input'
                     name='time-slot'
-                    placeholder='e.g. Green turf arena'
+                    placeholder='e.g. 8AM-10AM'
                     autoComplete='off'
-                    className='w-full outline-none bg-transparent border border-gray-300 rounded-xl py-3 px-3 text-sm text-gray-950 caret-green-500 mt-3 focus:border-green-500 placeholder:text-gray-400'
+                    className='w-full outline-none bg-transparent border border-gray-200 rounded-md py-4 px-5 text-sm text-gray-950 caret-green-500 mt-3 focus:border-green-500 placeholder:text-gray-400'
                 />
-                <div className='w-full h-5'>
+                <div className='w-full h-6'>
                     <span className='text-sm text-red-500'></span>
                 </div>
             </label>
-            <label htmlFor='facility-hours'>
-                <span className='block text-base text-gray-950'>Hours</span>
+            <label htmlFor='hour'>
+                <span className='block text-sm text-gray-950 md:text-base'>
+                    Hours
+                </span>
                 <input
-                    id='facility-hours'
+                    id='hour'
                     type='text'
                     aria-label='input'
-                    name='facility-hours'
-                    placeholder='e.g. Green turf arena'
+                    name='hour'
+                    placeholder='e.g. 1 Hours'
                     autoComplete='off'
-                    className='w-full outline-none bg-transparent border border-gray-300 rounded-xl py-3 px-3 text-sm text-gray-950 caret-green-500 mt-3 focus:border-green-500 placeholder:text-gray-400'
+                    className='w-full outline-none bg-transparent border border-gray-200 rounded-md py-4 px-5 text-sm text-gray-950 caret-green-500 mt-3 focus:border-green-500 placeholder:text-gray-400'
                 />
-                <div className='w-full h-5'>
+                <div className='w-full h-6'>
                     <span className='text-sm text-red-500'></span>
                 </div>
             </label>
@@ -70,9 +83,9 @@ export default function BookingForm() {
                 type='submit'
                 role='button'
                 aria-label='button'
-                className='w-full py-3 px-3 bg-green-500 rounded-xl text-sm text-white cursor-pointer transition-colors duration-200 ease-linear hover:bg-green-400 mt-2'
+                className='w-full py-4 px-8 bg-green-500 rounded-md text-sm text-white cursor-pointer transition-colors duration-200 ease-linear hover:bg-green-400 mt-2 md:text-base'
             >
-                Publish Facility
+                Book
             </button>
         </form>
     );
